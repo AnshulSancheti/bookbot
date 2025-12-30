c2a6758b2e4e8e77ad8f58ede657da95b5f62b7c
AnshulSancheti <anshul.sancheti007@gmail.com>
2025-12-30 12:18:05 +0000

    opened a file with python

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print ("greetings boot")
+def main():
+    book = get_books_text("/home/intelligentape/bookbot/books/frankenstein.txt")
+    print(book)
+
+
+
+def get_books_text(file_path):
+    with open (file_path) as f:
+        file_contents = f.read()
+        return file_contents
+    
+main()
